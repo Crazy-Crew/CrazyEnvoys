daf23675392f1eb429c19c5b98da486d17f706b1
Ryder Belserion <github@corecraft.me>
2022-07-06 21:16:44 -0400

    Disable metrics if they choose to.

--- plugin/src/main/java/com/badbones69/crazyenvoys/CrazyEnvoys.java
+++ plugin/src/main/java/com/badbones69/crazyenvoys/CrazyEnvoys.java
@@ -2,17 +2,22 @@ package com.badbones69.crazyenvoys;
 
 import com.badbones69.crazyenvoys.api.CrazyManager;
 import com.badbones69.crazyenvoys.api.FileManager;
+import com.badbones69.crazyenvoys.api.FileManager.Files;
 import com.badbones69.crazyenvoys.api.enums.Messages;
 import com.badbones69.crazyenvoys.api.events.EnvoyEndEvent;
 import com.badbones69.crazyenvoys.api.events.EnvoyEndEvent.EnvoyEndReason;
 import com.badbones69.crazyenvoys.commands.EnvoyCommand;
 import com.badbones69.crazyenvoys.commands.EnvoyTab;
-import com.badbones69.crazyenvoys.controllers.*;
+import com.badbones69.crazyenvoys.controllers.EditControl;
+import com.badbones69.crazyenvoys.controllers.EnvoyControl;
+import com.badbones69.crazyenvoys.controllers.FireworkDamageAPI;
+import com.badbones69.crazyenvoys.controllers.FlareControl;
 import com.badbones69.crazyenvoys.multisupport.MVdWPlaceholderAPISupport;
 import com.badbones69.crazyenvoys.multisupport.PlaceholderAPISupport;
 import com.badbones69.crazyenvoys.multisupport.Support;
 import com.badbones69.crazyenvoys.multisupport.ServerProtocol;
 import com.badbones69.crazyenvoys.multisupport.holograms.HolographicSupport;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
@@ -46,7 +51,21 @@ public class CrazyEnvoys extends JavaPlugin implements Listener {
         pluginManager.registerEvents(new EditControl(), this);
         pluginManager.registerEvents(new EnvoyControl(), this);
         pluginManager.registerEvents(new FlareControl(), this);
-        
+
+        boolean metricsEnabled = Files.CONFIG.getFile().getBoolean("Settings.Toggle-Metrics");
+
+        if (Files.CONFIG.getFile().getString("Settings.Toggle-Metrics") != null) {
+            if (metricsEnabled) new Metrics(this, 4537);
+        } else {
+            getLogger().warning("Metrics was automatically enabled.");
+            getLogger().warning("Please add Toggle-Metrics: false to the top of your config.yml");
+            getLogger().warning("https://github.com/Crazy-Crew/Crazy-Crates/blob/main/src/main/resources/config.yml");
+
+            getLogger().warning("An example if confused is linked above.");
+
+            new Metrics(this, 4537);
+        }
+
         try {
             if (ServerProtocol.isNewer(ServerProtocol.v1_10_R1)) {
                 pluginManager.registerEvents(new FireworkDamageAPI(), this);
