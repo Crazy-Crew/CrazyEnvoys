28c7bcbdaec62e40d5b6ccd305da4a7ebfeed5e7
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-03 22:25:24 -0400

    Add proper start/end comments

--- plugin/src/main/java/com/badbones69/crazyenvoys/api/CrazyManager.java
+++ plugin/src/main/java/com/badbones69/crazyenvoys/api/CrazyManager.java
@@ -652,7 +652,11 @@ public class CrazyManager {
             maxSpawns = envoySettings.getMaxCrates();
         } else if (envoySettings.isRandomAmount()) {
             // Generates a random number between the min and max settings
-            maxSpawns = this.random.nextInt(envoySettings.getMaxCrates() + 1 - envoySettings.getMinCrates()) + envoySettings.getMinCrates();
+
+            // Ryder start
+            maxSpawns = (int) Math.floor(Math.random()*(envoySettings.getMaxCrates()-envoySettings.getMinCrates()+1)+envoySettings.getMinCrates());
+            //maxSpawns = this.random.nextInt(envoySettings.getMaxCrates() + 1 - envoySettings.getMinCrates()) + envoySettings.getMinCrates();
+            // Ryder end
         } else {
             maxSpawns = envoySettings.isRandomLocationsEnabled() ? envoySettings.getMaxCrates() : spawnedLocations.size();
         }
