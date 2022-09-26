1a82d7995bd5840d382db9bad1e4502bf10b10f6
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-09-26 17:19:00 -0400

    Use PDC instead of changing metadata

--- plugin/src/main/java/com/badbones69/crazyenvoys/controllers/FireworkDamageAPI.java
+++ plugin/src/main/java/com/badbones69/crazyenvoys/controllers/FireworkDamageAPI.java
@@ -2,6 +2,7 @@ package com.badbones69.crazyenvoys.controllers;
 
 import com.badbones69.crazyenvoys.api.CrazyManager;
 import com.badbones69.crazyenvoys.multisupport.ServerProtocol;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
@@ -10,6 +11,8 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +21,17 @@ public class FireworkDamageAPI implements Listener {
 
     public static CrazyManager crazyManager = CrazyManager.getInstance();
 
+    static NamespacedKey noDamage = new NamespacedKey(crazyManager.getPlugin(), "no-damage");
+
     /**
      * @param firework The firework you want to add.
      */
     public static void addFirework(Entity firework) {
-        if (ServerProtocol.isNewer(ServerProtocol.v1_10_R1)) firework.setMetadata("nodamage", new FixedMetadataValue(crazyManager.getPlugin(), true));
+        if (ServerProtocol.isNewer(ServerProtocol.v1_10_R1)) {
+            PersistentDataContainer container = firework.getPersistentDataContainer();
+
+            container.set(noDamage, PersistentDataType.STRING, "no-damage");
+        }
     }
 
     // Ryder Start
@@ -30,7 +39,10 @@ public class FireworkDamageAPI implements Listener {
     public void onPlayerDamage(EntityDamageByEntityEvent e) {
         if (e.getDamager() instanceof Firework) {
             Firework fw = (Firework) e.getDamager();
-            if (fw.hasMetadata("nodamage")) e.setCancelled(true);
+
+            PersistentDataContainer container = fw.getPersistentDataContainer();
+
+            if (container.has(noDamage, PersistentDataType.STRING)) e.setCancelled(true);
         }
     }
     // Ryder End
