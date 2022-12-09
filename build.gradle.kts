178e864c4042e0a29c80f359396045af4dfe71bd
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-09 12:17:49 -0500

    Remove version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 
 val buildNumber: String? = System.getenv("BUILD_NUMBER")
 
-val jenkinsVersion = "1.4.18-b$buildNumber"
+val jenkinsVersion = "1.4.19-b$buildNumber"
 
 group = "com.badbones69.crazyenvoys"
-version = "1.4.18"
+version = "1.4.19"
 description = "Drop custom crates with any prize you want all over spawn for players to fight over."
 
 repositories {
@@ -20,14 +20,14 @@ repositories {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 
     /**
-     * Spigot Team
-     */
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-
-    /**
      * NBT API
      */
     maven("https://repo.codemc.org/repository/maven-public/")
+
+    /**
+     * Paper Team
+     */
+    maven("https://repo.papermc.io/repository/maven-public/")
 
     /**
      * EngineHub Team
@@ -43,22 +43,24 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    implementation("de.tr7zw", "nbt-data-api", "2.10.0")
 
-    compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.9")
+    implementation("org.bstats", "bstats-bukkit", "3.0.0")
 
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.1.0-SNAPSHOT")
+    compileOnly("io.papermc.paper", "paper-api", "1.19.3-R0.1-SNAPSHOT")
 
-    compileOnly("com.github.decentsoftware-eu:decentholograms:2.7.2")
+    compileOnly("me.filoghost.holographicdisplays", "holographicdisplays-api", "3.0.0")
 
-    compileOnly("me.clip:placeholderapi:2.11.2") {
+    compileOnly("com.github.decentsoftware-eu", "decentholograms", "2.7.7")
+
+    compileOnly("com.sk89q.worldguard", "worldguard-bukkit", "7.1.0-SNAPSHOT")
+
+    compileOnly("me.clip", "placeholderapi", "2.11.2") {
         exclude(group = "org.spigotmc", module = "spigot")
         exclude(group = "org.bukkit", module = "bukkit")
     }
 
-    implementation("de.tr7zw:nbt-data-api:2.10.0")
-
-    implementation("org.bstats:bstats-bukkit:3.0.0")
+    compileOnly("com.github.MilkBowl", "VaultAPI", "1.7")
 }
 
 java {
