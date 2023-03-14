cd8f967b5ad513d3821bb7029c3aee8187eeabe7
Ryder Belserion <no-reply@ryderbelserion.com>
2023-03-14 11:51:48 -0400

    Update changelog

--- platforms/paper/build.gradle.kts
+++ platforms/paper/build.gradle.kts
@@ -86,6 +86,7 @@ tasks {
             """
                 <h4>Changes:</h4>
                  <p>Added 1.19.4 support</p>
+                 <p>Removed 1.18.2 and below support</p>
                 <h4>Under the hood changes</h4>
                  <p>Simplified build script</p>
                 <h4>Bug Fixes:</h4>
