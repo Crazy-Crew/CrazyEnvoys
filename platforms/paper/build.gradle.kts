a03f5d31d559862a5230ecd9ffc507b514bad994
Ryder Belserion <no-reply@ryderbelserion.com>
2023-03-30 02:58:47 -0400

    Uncomment credentials

--- platforms/paper/build.gradle.kts
+++ platforms/paper/build.gradle.kts
@@ -119,8 +119,8 @@ publishing {
         val repo = if (beta) "beta" else "releases"
         maven("https://repo.crazycrew.us/$repo") {
             name = "crazycrew"
-            // Used for locally publishing.
-            // credentials(PasswordCredentials::class)
+
+            credentials(PasswordCredentials::class)
 
             credentials {
                 username = System.getenv("REPOSITORY_USERNAME")
