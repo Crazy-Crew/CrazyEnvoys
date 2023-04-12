2fe1e116ef3b88010768b6c5e2288aa003a8545f
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-12 00:16:12 -0400

    Update misc stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.lordcodes.turtle.shellRun
 import task.WebhookExtension
 import java.awt.Color
 
@@ -18,18 +17,13 @@ val extension = settings.versions.extension.get()
 val color = if (beta) betaUpdate else releaseUpdate
 val repo = if (beta) "beta" else "releases"
 
-val download = if (beta) "https://ci.crazycrew.us/job/${rootProject.name}/" else "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
+val download = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
 
-val msg = if (beta) "New version of ${rootProject.name} is ready!" else "New version of ${rootProject.name} is ready! <@&929463450214735912>"
+val msg = "New version of ${rootProject.name} is ready! <@&929463450214735912>"
 
-val hash = shellRun("git", listOf("rev-parse", "--short", "HEAD"))
+rootProject.version = "1.4.20.4"
 
-rootProject.version = if (beta) hash else "1.4.20.4"
-
-val desc = if (beta) """
-    Changes:
-    » N/A
-""".trimIndent() else "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
+val desc = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
 
 webhook {
     this.avatar("https://en.gravatar.com/avatar/${WebhookExtension.Gravatar().md5Hex("no-reply@ryderbelserion.com")}.jpeg")
@@ -66,7 +60,11 @@ webhook {
 
             this.title("What changed?")
 
-            this.description(desc)
+            this.description("""
+                > 
+                
+                Full Changelog -> $desc
+            """.trimIndent())
         }
     }
 
