c1575dab702435875b35df1386c7451f9740c02e
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-21 18:27:18 -0400

    Add common module

---
+++ common/src/main/java/us/crazycrew/crazyenvoys/common/api/AbstractPlugin.java
@@ -0,0 +1,9 @@
+package us.crazycrew.crazyenvoys.common.api;
+
+import us.crazycrew.crazyenvoys.common.config.ConfigManager;
+
+public abstract class AbstractPlugin {
+
+    public abstract ConfigManager getConfigManager();
+
+}
