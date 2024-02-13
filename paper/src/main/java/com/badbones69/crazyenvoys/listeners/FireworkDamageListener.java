975668cc093931bda292b857e2a23b042d7dfaec
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-13 18:13:27 -0500

    Update firework damage listener

--- paper/src/main/java/com/badbones69/crazyenvoys/listeners/FireworkDamageListener.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/listeners/FireworkDamageListener.java
@@ -1,21 +1,25 @@
 package com.badbones69.crazyenvoys.listeners;
 
-import com.badbones69.crazyenvoys.api.enums.DataKeys;
+import com.badbones69.crazyenvoys.api.enums.PersistentKeys;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 
 public class FireworkDamageListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Firework firework) {
+    public void onFireworkDamage(EntityDamageEvent event) {
+        Entity directEntity = event.getDamageSource().getDirectEntity();
+
+        if (directEntity instanceof Firework firework) {
             PersistentDataContainer container = firework.getPersistentDataContainer();
 
-            if (container.has(DataKeys.NO_FIREWORK_DAMAGE.getKey(), PersistentDataType.BOOLEAN)) event.setCancelled(true);
+            if (container.has(PersistentKeys.no_firework_damage.getNamespacedKey())) {
+                event.setCancelled(true);
+            }
         }
     }
 }
