5977bf94d50fa469024f2c779ac83ec67ed939d8
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-03 02:38:35 -0400

    update persistent keys

--- paper/src/main/java/com/badbones69/crazyenvoys/api/enums/PersistentKeys.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/api/enums/PersistentKeys.java
@@ -2,29 +2,22 @@ package com.badbones69.crazyenvoys.api.enums;
 
 import com.badbones69.crazyenvoys.CrazyEnvoys;
 import org.bukkit.NamespacedKey;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public enum PersistentKeys {
 
-    no_firework_damage("firework", PersistentDataType.BOOLEAN);
+    no_firework_damage("firework"); // boolean
 
     private final @NotNull CrazyEnvoys plugin = JavaPlugin.getPlugin(CrazyEnvoys.class);
 
     private final String NamespacedKey;
-    private final PersistentDataType type;
 
-    PersistentKeys(String NamespacedKey, PersistentDataType type) {
+    PersistentKeys(String NamespacedKey) {
         this.NamespacedKey = NamespacedKey;
-        this.type = type;
     }
 
     public NamespacedKey getNamespacedKey() {
         return new NamespacedKey(this.plugin, this.plugin.getName().toLowerCase() + "_" + this.NamespacedKey);
-    }
-
-    public PersistentDataType getType() {
-        return this.type;
     }
 }
