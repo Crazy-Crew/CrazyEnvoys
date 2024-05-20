8389d329fbeeedf274b030e5dd5c4d707e4fbc52
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-20 13:29:53 -0400

    add getter for tier name

--- paper/src/main/java/com/badbones69/crazyenvoys/api/objects/misc/v2/Tier.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/api/objects/misc/v2/Tier.java
@@ -97,6 +97,10 @@ public class Tier {
         return this.bundles;
     }
 
+    public final String getTierName() {
+        return this.tierName;
+    }
+
     public final Material getBlock() {
         return this.block;
     }
