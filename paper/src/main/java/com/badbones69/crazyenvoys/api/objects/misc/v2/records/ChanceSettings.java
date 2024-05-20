1fdb08af187b97bb8b3ca372a6c9024b6b5e3b86
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-20 02:05:52 -0400

    add a testing random() method

--- paper/src/main/java/com/badbones69/crazyenvoys/api/objects/misc/v2/records/ChanceSettings.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/api/objects/misc/v2/records/ChanceSettings.java
@@ -1,3 +1,26 @@
 package com.badbones69.crazyenvoys.api.objects.misc.v2.records;
 
-public record ChanceSettings(boolean useChance, int maxRange, int chance) {}
+import java.util.concurrent.ThreadLocalRandom;
+
+public record ChanceSettings(boolean useChance, int maxRange, int chance) {
+
+    /**
+     * A temporary method to test chance percentages. This will likely change a lot as I do more research
+     * I want to simulate as random as possible and potentially include a weight system.
+     *
+     * @return true or false
+     */
+    public boolean random() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        for (int index = 0; index < 100; index++) {
+            final int number = random.nextInt(maxRange);
+
+            if (number >= 1 && number <= chance) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
