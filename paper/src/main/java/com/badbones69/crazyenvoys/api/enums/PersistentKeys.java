615ad52443711f00ff0f16fbcd066ae9a313ac3d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-04 02:43:08 -0400

    Remove raw usage of PersistentDataType from the enum

--- paper/src/main/java/com/badbones69/crazyenvoys/api/enums/PersistentKeys.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/api/enums/PersistentKeys.java
@@ -2,28 +2,21 @@ package com.badbones69.crazyenvoys.api.enums;
 
 import com.badbones69.crazyenvoys.CrazyEnvoys;
 import org.bukkit.NamespacedKey;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public enum PersistentKeys {
 
-    no_firework_damage("firework", PersistentDataType.BOOLEAN);
+    no_firework_damage("firework");
 
     private final @NotNull CrazyEnvoys plugin = CrazyEnvoys.get();
 
     private final String NamespacedKey;
-    private final PersistentDataType type;
 
-    PersistentKeys(String NamespacedKey, PersistentDataType type) {
+    PersistentKeys(String NamespacedKey) {
         this.NamespacedKey = NamespacedKey;
-        this.type = type;
     }
 
     public NamespacedKey getNamespacedKey() {
         return new NamespacedKey(this.plugin, this.plugin.getName().toLowerCase() + "_" + this.NamespacedKey);
-    }
-
-    public PersistentDataType getType() {
-        return this.type;
     }
 }
