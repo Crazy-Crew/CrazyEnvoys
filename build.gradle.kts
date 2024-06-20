8941726314367f9ed65c86c723e8a2493d93dcb6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-20 17:06:45 -0400

    [ci-skip] oopsie

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 val buildNumber: String? = System.getenv("BUILD_NUMBER")
 
-rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "1.10.0"
+rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "1.10"
 
 val isSnapshot = false
 
