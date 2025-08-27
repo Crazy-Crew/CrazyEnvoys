f999927c16d5ce22a6ab88fc1fde237252f25240
InstantlyMoist <kyllian007@gmail.com>
2025-08-27 20:10:44 +0200

    Fix seconds always showing

--- paper/src/main/java/com/badbones69/crazyenvoys/Methods.java
+++ paper/src/main/java/com/badbones69/crazyenvoys/Methods.java
@@ -186,7 +186,7 @@ public class Methods {
         if (day > 0) message += day + Messages.day.getMessage() + ", ";
         if (day > 0 || hour > 0) message += hour + Messages.hour.getMessage() + ", ";
         if (day > 0 || hour > 0 || minute > 0) message += minute + Messages.minute.getMessage() + ", ";
-        if (day > 0 || hour > 0 || minute > 0 || second > 0) message += second + Messages.second.getMessage() + ", ";
+        if (second > 0) message += second + Messages.second.getMessage() + ", ";
 
         if (message.length() < 2) {
             message = "0" + Messages.second.getMessage();
